b453cf85c8e39f61d3d55ec5ea031955707ddfb2
Szymon Konopek <sk.gornik@gmail.com>
2022-03-10 21:34:20 +0100

    Final Product

--- app.py
+++ app.py
@@ -334,6 +334,11 @@ def download_json(id):
             mimetype='application/json',
             headers={'Content-Disposition':f'attachment;filename={id}.json'})
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(debug = True)
 
