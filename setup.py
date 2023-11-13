210dcb2696305b2d6c661f506c4b9c4dcbafec0a
szymonkonopek <sk.gornik@gmail.com>
2023-11-13 22:25:54 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Ceneo-Webscraper',
+    version='1.0',
+    packages=[''],
+    url='ceneo.pl',
+    license='',
+    author='szymon',
+    author_email='szymon.konopek@gmail.com',
+    description='Ceneo scraper'
+)
